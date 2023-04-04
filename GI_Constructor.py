fc7842c4af5e63c37e8da245c1d4f9425deca68c
AdrianElija <102810263+AdrianElija@users.noreply.github.com>
2023-04-04 01:34:37 -0400

    Add files via upload

--- GI_Constructor.py
+++ GI_Constructor.py
@@ -6,15 +6,11 @@ import arcpy
 import sys
 import os
 # Get input parameters
-#Parcels_folder = r"C:\Users\adriansantiago\Documents\AES Capstone\Unzipped data\Parcels_Unzipped"
 Parcels_folder = arcpy.GetParameterAsText(0)
 CENACS_folder = arcpy.GetParameterAsText(1)
 Geography_Baseline = arcpy.GetParameterAsText(2)
 Study_subject = arcpy.GetParameterAsText(3)
 Output_Folder_Location = arcpy.GetParameterAsText(4)
-
-arcpy.AddMessage(Parcels_folder)
-arcpy.AddMessage(CENACS_folder)
 
 # Create output folder if it doesn't exist
 new_folder_name = "New_run"
@@ -22,26 +18,17 @@ Output_File_Location = os.path.join(Output_Folder_Location, new_folder_name)
 if not os.path.exists(Output_File_Location):
     os.makedirs(Output_File_Location)
 
-# Find all Polygon feature classes in all File Geodatabases in Parcels_folder
+# Get list of feature classes in the input Geodatabase from Parcels_folder
 arcpy.env.workspace = Parcels_folder
 Parcels_list = arcpy.ListFeatureClasses()
+arcpy.AddMessage(f"Found {len(Parcels_list)} Parcels files:")
 arcpy.AddMessage(Parcels_list)
-
-# Use the first 10 files in the list
-Parcels_list = Parcels_list[:10]
-
-# Assign the first 10 values to Parcels1, Parcels2, etc.
-for i in range(10):
-    if len(Parcels_list) > i:
-        exec(f"parcels{i+1} = '{Parcels_list[i]}'")
-
 if len(Parcels_list) == 0:
     arcpy.AddMessage("No feature classes found in workspace")
 else:
     arcpy.AddMessage("Found feature classes:")
     for fc in Parcels_list:
         arcpy.AddMessage(fc)
-arcpy.AddMessage(Parcels_list)
 
 # Find all Polygon feature classes in all File Geodatabases in CENACS_folder
 arcpy.env.workspace = CENACS_folder
@@ -51,9 +38,6 @@ CENACS_list = arcpy.ListFeatureClasses()
 arcpy.AddMessage(f"Found {len(CENACS_list)} CENACS files:")
 arcpy.AddMessage(CENACS_list)
 
-# Use the first 10 files in the list
-CENACS_list = CENACS_list[:10]
-
 if len(CENACS_list) == 0:
     arcpy.AddMessage("No feature classes found in workspace")
 else:
@@ -61,47 +45,31 @@ else:
     for fc in CENACS_list:
         arcpy.AddMessage(fc)
 arcpy.AddMessage(CENACS_list)
-
-
-# Assign the first 10 values to Parcels1, Parcels2, etc. and CENACS1, CENACS2, etc.
-for i in range(10):
-    if len(Parcels_list) > i:
-        exec(f"parcels{i+1} = '{Parcels_list[i]}'")
-    if len(CENACS_list) > i:
-        exec(f"cenacs{i+1} = '{CENACS_list[i]}'")
-
-arcpy.AddMessage(CENACS_list)
 arcpy.AddMessage(Parcels_list)
 
-
 def createanalysisgeos(CENACS_list, Output_File_Location,Study_subject):
-    studybuffer = os.path.join(Output_File_Location, "study_buffer")
+    studybuffer = os.path.join(Output_File_Location, "\study_buffer")
     buffer_distance_or_field = "0.5 Miles"
     sideType = "FULL"
     endType = "ROUND"
     #Create 0.5 mile buffer
     arcpy.Buffer_analysis(Study_subject, studybuffer, buffer_distance_or_field, sideType, endType)
-
     for cenacs in CENACS_list:
         # Select by location to select block groups within geography
         arcpy.SelectLayerByLocation_management(cenacs, "INTERSECT", Geography_Baseline)
         # Save features selected by Geography_Baseline to output location
-        out_feature_class1 = os.path.join(Output_File_Location, f"{cenacs}_baseline.shp")
+        out_feature_class1 = os.path.join(Output_File_Location, f"\{cenacs}_baseline")
         arcpy.CopyFeatures_management(cenacs, out_feature_class1)
         # Select by Location: intersect 0.5 mile buffer:
         arcpy.SelectLayerByLocation_management(cenacs, "INTERSECT", studybuffer)
-
         # Save features selected by 0.5 mile buffer to output location
-        studycenacs = os.path.join(Output_File_Location, f"{cenacs}_study_buffer.shp")
+        studycenacs = os.path.join(Output_File_Location, f"\{cenacs}_study_buffer")
         arcpy.CopyFeatures_management(cenacs, studycenacs)
 
-##############Prep the CENACS######################
-def modcenacs(CENACS_list, Output_File_Location):
-    # Save current workspace
-    orig_workspace = arcpy.env.workspace
-    # Set output workspace environment
-    arcpy.env.workspace = Output_File_Location
+#Prep the CENACS
 
+def modcenacs(CENACS_list, Output_File_Location):
+    arcpy.AddMessage(arcpy.env.workspace)
     # Define field names and data types
     field1 = "PCT_MIN"
     field2 = "PCT_ED_COLLEGE"
@@ -110,58 +78,39 @@ def modcenacs(CENACS_list, Output_File_Location):
         # Add fields
         arcpy.AddField_management(cenacs, field1, field_type)
         arcpy.AddField_management(cenacs, field2, field_type)
-
         # Calculate fields
         expression1 = "((!TOTALPOP!-!WHITE!)/!TOTALPOP!)*100.0"
         arcpy.CalculateField_management(cenacs, field1, expression1, "PYTHON_9.3")
-
         expression2 = "!ED_COLLEGE!/!TOTALPOP!"
         arcpy.CalculateField_management(cenacs, field2, expression2, "PYTHON_9.3")
-
         # Save modified dataset to output location
-        out_path = Output_File_Location
-        out_name = cenacs + ".shp"  # Or whatever filename you want to use
         arcpy.env.overwriteOutput = True
-        out_fc = arcpy.CopyFeatures_management(cenacs, os.path.join(out_path, out_name))
-        arcpy.env.workspace = orig_workspace
-
-
 
 def modparcels(Parcels_list, Output_File_Location, CENACS_list):
+    arcpy.AddMessage(arcpy.env.workspace)
     # Define where clauses to select residential and nonresidential parcels
     where_clauses = ["LU_RES = 'YES'", "NOT LU_RES = 'YES'"]
-
     for parcels in Parcels_list:
         arcpy.AddMessage(parcels)
         arcpy.SelectLayerByLocation_management(parcels, 'HAVE_THEIR_CENTER_IN', Geography_Baseline)
         # Loop over the where clauses to select residential and nonresidential parcels
         for j, where_clause in enumerate(where_clauses):
             arcpy.SelectLayerByAttribute_management(parcels, "NEW_SELECTION", where_clause)
-
             # Save selected parcels to output location
-            out_feature_class = os.path.join(Output_File_Location, f"{os.path.basename(parcels)}_{'residential' if j == 0 else 'nonresidential'}.shp")
+            out_feature_class = os.path.join(Output_File_Location, f"\\{os.path.basename(parcels)}_{'residential' if j == 0 else 'nonresidential'}out_test")
             arcpy.CopyFeatures_management(parcels, out_feature_class)
-
             # Summarize parcels attributes within census block groups
-            for cenacs in CENACS_list:
-                studycenacs_list = [f"{cenacs}_study_buffer.shp"]
-
-            for studyCENACS in studycenacs_list:
-
-                in_polygons = studyCENACS
+            for CENACS in CENACS_list:
+                in_polygons = CENACS
                 in_sum_features = out_feature_class  # Use the selected parcel layer
-                out_feature_class2 = os.path.join(Output_File_Location, f"{os.path.basename(parcels)}_{'residential' if j == 0 else 'nonresidential'}_summary.shp")
+                out_feature_class2 = os.path.join(Output_File_Location, f"\\{os.path.basename(parcels)}_{'residential' if j == 0 else 'nonresidential'}_summary")
                 sum_fields = ["JV"]
                 arcpy.SummarizeWithin_analysis(in_polygons, in_sum_features, out_feature_class2, "KEEP_ALL", f"{sum_fields[0]} SUM;{sum_fields[0]} MEAN")
-
                 # Rename and move the output summary table to the desired location
-                out_table = os.path.join(Output_File_Location, f"{os.path.basename(parcels)}_{'residential' if j == 0 else 'nonresidential'}_JV_summary.dbf")
+                out_table = os.path.join(Output_File_Location, f"\\{os.path.basename(parcels)}_{'residential' if j == 0 else 'nonresidential'}_JV_summary.dbf")
                 arcpy.TableToTable_conversion(out_feature_class2, Output_File_Location, os.path.basename(out_table))
-
         # Print a message for each processed feature class
-        arcpy.AddMessage(f"Processed {os.path.basename(parcels)} ({i + 1}/{len(Parcels_list)})")
-
-
+        arcpy.AddMessage(f"Processed {os.path.basename(parcels)})")
 
 
 def datacalcandpctchg(CENACS_list, Output_File_Location):
@@ -200,8 +149,6 @@ def datacalcandpctchg(CENACS_list, Output_File_Location):
     Total_output_table = pd.concat(output_tables)
     df = Total_output_table
     return df
-
-
 
 ## Index Construction
 def indexconstructor(indicators_table, Output_File_Location):
@@ -246,8 +193,8 @@ def indexconstructor(indicators_table, Output_File_Location):
 # Create the file geodatabase at the output location
 #arcpy.CreateFileGDB_management(Output_File_Location, "output.gdb")
 
-createanalysisgeos(CENACS_list, Output_File_Location, Study_subject)
 modcenacs(CENACS_list, Output_File_Location)
+createanalysisgeos(CENACS_list, Output_File_Location, Study_subject)
 modparcels(Parcels_list, Output_File_Location, CENACS_list)
 datacalcandpctchg(CENACS_list, Output_File_Location)
 indicators_table = datacalcandpctchg(Output_File_Location, CENACS_list)
